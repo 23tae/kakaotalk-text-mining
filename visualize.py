379659b5dc091ca89ce24a2c2c4730eba19799c2
23tae <taehoonkim.dev@gmail.com>
2023-11-21 17:49:16 +0900

    Fix: 빈 이미지 저장되는 문제 해결

--- visualize.py
+++ visualize.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-def visualize_length(filtered_conversations):
+def visualize_length(filtered_conversations, output_path):
   message_lengths = [len(message) for message in filtered_conversations]
 
   plt.hist(message_lengths, bins=20, edgecolor='black')
   plt.xlabel('Message Length')
   plt.ylabel('Frequency')
   plt.title('Distribution of Message Lengths')
-  plt.savefig('./result/length_result.png')
-  plt.show()
+  plt.savefig(output_path + '_length.png')
 
-def visualize_wordcloud(filtered_conversations):
+def visualize_wordcloud(filtered_conversations, output_path):
   all_words = [word for message in filtered_conversations for word in message]
   text = ' '.join(all_words)
   wordcloud = WordCloud(
@@ -21,5 +20,4 @@ def visualize_wordcloud(filtered_conversations):
   plt.figure(figsize=(10, 5))
   plt.imshow(wordcloud, interpolation='bilinear')
   plt.axis('off')
-  plt.savefig('./result/wordcloud_result.png')
-  plt.show()
+  plt.savefig(output_path + '_wordcloud.png')
