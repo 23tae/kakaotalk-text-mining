03822afd00c3ac211879d79e66e94ef1d6920dc6
23tae <taehoonkim.dev@gmail.com>
2023-11-21 17:50:10 +0900

    Feat: 출력 파일명 생성

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import sys
 from prerequisite import prerequisite
 from preprocess import preprocess
 from visualize import visualize_length, visualize_wordcloud
+import os
+
+def get_filename(file_path):
+  dirname, basename = os.path.split(file_path)
+  name, ext = os.path.splitext(basename)
+  return name
+
+def get_output_path_without_ext(input_path):
+  filename = get_filename(input_path)
+  return os.path.join('./result', filename)
 
 def main(path: str):
+    try:
+       os.path.isfile(path)
+    except:
+        raise Exception('File not found')
     df = prerequisite(path)
     filtered_conversations = preprocess(df)
-    visualize_length(filtered_conversations)
-    visualize_wordcloud(filtered_conversations)
+    output_path = get_output_path_without_ext(path)
+    visualize_length(filtered_conversations, output_path)
+    visualize_wordcloud(filtered_conversations, output_path)
 
 if __name__ == "__main__":
     main(sys.argv[1])
